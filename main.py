24d2c4c218d097adb3b68c79c3b62cc096ead1cb
zakiy farhan <zakiyff@gmail.com>
2020-08-28 00:00:51 +0700

    Tampilan sudah bisa, namun belum rapi

--- main.py
+++ main.py
@@ -19,13 +19,28 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
 async def home(request : Request):
-    return templates.TemplateResponse("home.php",{
+
+    return templates.TemplateResponse("home.html",{
         "request" : request
     })
 
+# UPLOAD dan CONVERSION
+@app.post("/result")
+async def create_upload_file(request : Request , anime1: str = File(...), anime2 : str = File(...)):
+    reco = mal_scrap.big_list_recommendation([anime1, anime2])
+    arr_reco = []
+    for i in reco:
+
+        print(i)
+        arr_reco.append([mal_scrap.get_title(i), reco[i][0], reco[i][1]])
+
+    return templates.TemplateResponse("download.html",{
+      "request" : request,
+      "data" : arr_reco
+      })
 
 
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host = "172.0.0.1", port = 5001
+    uvicorn.run(app, host = "127.0.0.1", port = 5001)
